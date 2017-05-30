2c274b56cc29218ee96437f8874e4123c7ac09b7
timfesta <timothyfesta@yahoo.com>
2017-05-30 05:56:23 -0700

    added content to coit & ferry

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'goldengate' => 'franciscos#goldengate'
   get 'coit' => 'franciscos#coit'
+  get 'ferrybuilding' => 'franciscos#ferrybuilding'
   resources :franciscos
 
   root 'statics#home'
