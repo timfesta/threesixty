5d870e98cf6c3288df92baadebf7e0f7a71e13cc
timfesta <timothyfesta@yahoo.com>
2017-05-30 06:06:25 -0700

    added sutro path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'goldengate' => 'franciscos#goldengate'
   get 'coit' => 'franciscos#coit'
   get 'ferrybuilding' => 'franciscos#ferrybuilding'
+  get 'sutro' => 'franciscos#sutro'
   resources :franciscos
 
   root 'statics#home'
