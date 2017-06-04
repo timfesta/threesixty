8b6732f32388ab242873e1879220f6094529e5f1
timfesta <timothyfesta@yahoo.com>
2017-06-03 20:01:24 -0700

    added new links

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'coit' => 'franciscos#coit'
   get 'ferrybuilding' => 'franciscos#ferrybuilding'
   get 'fivefivefive' => 'franciscos#fivefivefive'
+  get 'goldengate' => 'franciscos#goldengate'
+  get 'att' => 'franciscos#att'
+  get 'baybridge' => 'franciscos#baybridge'
+  get 'sutro' => 'franciscos#sutro'
   resources :franciscos
 
   root 'statics#home'
